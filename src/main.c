19b4768674cee559b692800770ac268f24c9c98f
Felipe Andrioli <felipeandrioli56@hotmail.com>
2023-01-03 13:28:50 -0300

    Added window name; Setting window to fullscreen

--- src/main.c
+++ src/main.c
@@ -17,10 +17,16 @@ bool initialize_window(void) {
         return false;
     }
 
+    // Query and set fullscreen resolution
+    SDL_DisplayMode display_mode;
+    SDL_GetCurrentDisplayMode(0, &display_mode);
+    window_width = display_mode.w;
+    window_height = display_mode.h;
+
     // Create a SDL Window
-    window = SDL_CreateWindow(NULL, SDL_WINDOWPOS_CENTERED, 
-        SDL_WINDOWPOS_CENTERED, window_width, window_height, 
-        SDL_WINDOW_BORDERLESS);
+    window = SDL_CreateWindow("ScratchRenderingEngine.exe", 
+        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, window_width, 
+        window_height, SDL_WINDOW_BORDERLESS);
 
     if (!window) {
         fprintf(stderr, "Error creating SDL window\n");
@@ -34,6 +40,8 @@ bool initialize_window(void) {
         fprintf(stderr, "Error creating SDL Renderer\n");
         return false;
     }
+
+    SDL_SetWindowFullscreen(window, SDL_WINDOW_FULLSCREEN);
 
     return true;
 }
