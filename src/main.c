89e3d1c584bb0ec843e949eae1f221ff497ee8c7
Felipe Andrioli <felipeandrioli56@hotmail.com>
2023-01-03 21:30:43 -0300

    Refactoring

--- src/main.c
+++ src/main.c
@@ -3,48 +3,9 @@
 #include <stdbool.h>
 #include <SDL2/SDL.h>
 
-SDL_Window* window = NULL;
-SDL_Renderer* renderer = NULL;
-SDL_Texture* color_buffer_texture = NULL;
-uint32_t *color_buffer = NULL;
+#include "../include/display.h"
+
 bool is_running = false;
-int window_width = 800;
-int window_height = 600;
-
-bool initialize_window(void) {
-    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
-        fprintf(stderr, "Error initializing SDL\n");
-        return false;
-    }
-
-    // Query and set fullscreen resolution
-    SDL_DisplayMode display_mode;
-    SDL_GetCurrentDisplayMode(0, &display_mode);
-    window_width = display_mode.w;
-    window_height = display_mode.h;
-
-    // Create a SDL Window
-    window = SDL_CreateWindow("ScratchRenderingEngine.exe", 
-        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, window_width, 
-        window_height, SDL_WINDOW_BORDERLESS);
-
-    if (!window) {
-        fprintf(stderr, "Error creating SDL window\n");
-        return false;
-    }
-
-    // Create a SDL Renderer
-    renderer = SDL_CreateRenderer(window, -1, 0);
-
-    if (!renderer) {
-        fprintf(stderr, "Error creating SDL Renderer\n");
-        return false;
-    }
-
-    SDL_SetWindowFullscreen(window, SDL_WINDOW_FULLSCREEN);
-
-    return true;
-}
 
 void setup(void) {
     // Allocate the memory in bytes to holde the entire color buffer
@@ -85,50 +46,6 @@ void update(void) {
 
 }
 
-void render_color_buffer() {
-    SDL_UpdateTexture(color_buffer_texture, NULL, color_buffer, 
-        (int)(window_width * sizeof(uint32_t)));
-    SDL_RenderCopy(renderer, color_buffer_texture, NULL, NULL);
-}
-
-void clear_color_buffer(uint32_t color) {
-    for (int y = 0; y < window_height; y++) {
-        for (int x = 0; x < window_width; x++) {
-            color_buffer[(window_width * y) + x] = color;
-        }
-    }
-}
-
-void draw_grid_dots(int column, int row) {
-    for (int y = 0; y < window_height; y += row) {
-        for (int x = 0; x < window_width; x += column) {
-            color_buffer[(window_width * y) + x] = 0xFF333333;
-        }
-    }
-}
-
-void draw_grid_lines(int column, int row) {
-    for (int y = 0; y < window_height; y++) {
-        for (int x = 0; x < window_width; x++) {
-            if (x % column == 0 || y % row == 0) {
-                color_buffer[(window_width * y) + x] = 0xFF333333;
-            } else {
-                color_buffer[(window_width * y) + x] = 0xFF000000;
-            }
-        }
-    }
-}
-
-void draw_rect(int pos_x, int pos_y, int width, int height, uint32_t color) {
-    for (int i = 0; i < width; i++) {
-        for (int j = 0; j < height; j++) {
-            int current_x = pos_x + i;
-            int current_y = pos_y + j;
-            color_buffer[(window_width * current_y) + current_x] = color;
-        }
-    }
-}
-
 void render(void) {
     SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
     SDL_RenderClear(renderer);
@@ -140,13 +57,6 @@ void render(void) {
     clear_color_buffer(0xFF000000);
     
     SDL_RenderPresent(renderer);
-}
-
-void destroy_window(void) {
-    free(color_buffer);
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
 }
 
 int main(int argc, char *argv[]) {
