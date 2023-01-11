adc38e5f2794c69e8c242f099996b3cf064692cb
Felipe Andrioli <felipeandrioli56@hotmail.com>
2023-01-11 18:12:07 -0300

    Added function to draw line; Added function to draw triangles

--- include/display.h
+++ include/display.h
@@ -4,6 +4,7 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include <SDL2/SDL.h>
+#include "../include/triangle.h"
 
 #define FPS 30
 #define FRAME_TARGET_TIME (1000 / FPS)
@@ -24,6 +25,8 @@ void destroy_window(void);
 void draw_grid_dots(int column, int row);
 void draw_grid_lines(int column, int row);
 void draw_rect(int pos_x, int pos_y, int width, int height, uint32_t color);
+void draw_triangle(triangle_t triangle, uint32_t color);
+void draw_line(int x0, int y0, int x1, int y1, uint32_t color);
 void draw_pixel(int x, int y, uint32_t color);
 
 #endif
