c4ed59c156191d0a9535f60655f61fdde957531a
Felipe Andrioli <felipeandrioli56@hotmail.com>
2023-01-23 19:18:39 -0300

    Added normalization vector operation

--- src/vector.c
+++ src/vector.c
@@ -28,6 +28,12 @@ float vec2_dot(vec2_t a, vec2_t b) {
     return a.x * b.x + a.y * b.y;
 }
 
+void vec2_normalize(vec2_t *v) {
+    float mag = vec2_length(*v);
+    v->x /= mag;
+    v->y /= mag;
+}
+
 float vec3_length(vec3_t v) {
     return sqrt(v.x * v.x + v.y * v.y + v.z * v.z);
 }
@@ -64,6 +70,13 @@ vec3_t vec3_cross(vec3_t a, vec3_t b) {
 
 float vec3_dot(vec3_t a, vec3_t b) {
     return a.x * b.x + a.y * b.y + a.z * b.z;
+}
+
+void vec3_normalize(vec3_t *v) {
+    float mag = vec3_length(*v);
+    v->x /= mag;
+    v->y /= mag;
+    v->z /= mag;
 }
 
 vec3_t vec3_rotate_x(vec3_t v, float angle) {
