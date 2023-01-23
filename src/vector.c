2c8df78bf3c8da2ec5af568503e185cba5eb4c61
Felipe Andrioli <felipeandrioli56@hotmail.com>
2023-01-23 06:40:35 -0300

    Added vector magnitude operation

--- src/vector.c
+++ src/vector.c
@@ -1,5 +1,13 @@
 #include "../include/vector.h"
 
+float vec2_length(vec2_t v) {
+    return sqrt(v.x * v.x + v.y * v.y);
+}
+
+float vec3_length(vec3_t v) {
+    return sqrt(v.x * v.x + v.y * v.y + v.z * v.z);
+}
+
 vec3_t vec3_rotate_x(vec3_t v, float angle) {
     vec3_t rotated_vector = {
         v.x,
