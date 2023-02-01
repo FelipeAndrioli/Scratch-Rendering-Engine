607d24eba3f3f2f091345d76cdb969f0d0b4e3ba
Felipe Andrioli <felipeandrioli56@hotmail.com>
2023-02-01 06:37:40 -0300

    Using the rotation matrices to rotate our meshes

--- src/main.c
+++ src/main.c
@@ -106,18 +106,21 @@ void update(void) {
     // Initialize the array
     triangles_to_render = NULL;
 
-    //mesh.rotation.x += 0.01;
-    //mesh.rotation.y += 0.01;
-    //mesh.rotation.z += 0.01;
+    mesh.rotation.x += 0.01;
+    mesh.rotation.y += 0.01;
+    mesh.rotation.z += 0.01;
 
     //mesh.scale.x += 0.002;
     //mesh.scale.y += 0.001;
 
-    mesh.translation.x += 0.01;
+    //mesh.translation.x += 0.01;
     mesh.translation.z = 5.0;
     
     mat4_t scale_matrix = mat4_make_scale(&mesh.scale);
     mat4_t translation_matrix = mat4_make_translation(&mesh.translation);
+    mat4_t rotation_matrix_x = mat4_make_rotation_x(&mesh.rotation);
+    mat4_t rotation_matrix_y = mat4_make_rotation_y(&mesh.rotation);
+    mat4_t rotation_matrix_z = mat4_make_rotation_z(&mesh.rotation);
 
     int n_faces = array_length(mesh.faces);
     for (int i = 0; i < n_faces; i++) {
@@ -135,8 +138,15 @@ void update(void) {
         // Transformations 
         for (int j = 0; j < 3; j++) {
             vec4_t transformed_vertex = vec4_from_vec3(face_vertices[j]);
-           
+          
+            // transformations order 
+            // 1 - scale
+            // 2 - rotate
+            // 3 - translate
             transformed_vertex = mat4_mult_vec4(scale_matrix, transformed_vertex);
+            transformed_vertex = mat4_mult_vec4(rotation_matrix_x, transformed_vertex);
+            transformed_vertex = mat4_mult_vec4(rotation_matrix_y, transformed_vertex);
+            transformed_vertex = mat4_mult_vec4(rotation_matrix_z, transformed_vertex);
             transformed_vertex = mat4_mult_vec4(translation_matrix, transformed_vertex);
 
             transformed_vertices[j] = transformed_vertex;
