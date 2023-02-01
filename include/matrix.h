241c93d4cae2b5475c35c7113b4325abc03deabe
Felipe Andrioli <felipeandrioli56@hotmail.com>
2023-02-01 05:56:21 -0300

    Added scale matrix using pointer; Added translation matrix; Fixing matrix multiplication

--- include/matrix.h
+++ include/matrix.h
@@ -8,7 +8,8 @@ typedef struct {
 } mat4_t;
 
 mat4_t mat4_identity(void);
-mat4_t mat4_make_scale(float sx, float sy, float sz);
+mat4_t mat4_make_scale(vec3_t *scale);
+mat4_t mat4_make_translation(vec3_t *translation);
 vec4_t mat4_mult_vec4(mat4_t m, vec4_t v);
 
 #endif
