ddc0c7582bb1e705a4ef2b11526a8da67b3c238e
Felipe Andrioli <felipeandrioli56@hotmail.com>
2023-02-01 05:54:13 -0300

    Added support to mat4 and vec4 on logs

--- include/log.h
+++ include/log.h
@@ -4,10 +4,13 @@
 #include <stdio.h>
 
 #include "./vector.h"
+#include "./matrix.h"
 
 void log_const_char(const char* message);
 void log_int_value(int value);
 void log_double_value(float value);
 void log_vec3(vec3_t vec3);
+void log_vec4(vec4_t vec4);
+void log_mat4(mat4_t m);
 
 #endif
--- src/log.c
+++ src/log.c
@@ -15,3 +15,16 @@ void log_float_value(float value) {
 void log_vec3(vec3_t vec3) {
     printf("%f %f %f\n", vec3.x, vec3.y, vec3.z);
 }
+
+void log_vec4(vec4_t vec4) {
+    printf("%f %f %f %f\n", vec4.x, vec4.y, vec4.z, vec4.w);
+}
+
+void log_mat4(mat4_t m) {
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            printf("%f ", m.m[i][j]);
+        }
+        printf("\n");
+    }
+}
