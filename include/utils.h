09d68b05bb6e4affa819421b079d6687699c2fe7
Felipe Andrioli <felipeandrioli56@hotmail.com>
2023-02-09 20:58:07 -0300

    Added utils files

---
+++ include/utils.h
@@ -0,0 +1,7 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+void int_swap(int *a, int *b);
+void float_swap(float *a, float *b);
+
+#endif
---
+++ src/utils.c
@@ -0,0 +1,15 @@
+#include "../include/utils.h"
+
+void int_swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void float_swap(float *a, float *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+
