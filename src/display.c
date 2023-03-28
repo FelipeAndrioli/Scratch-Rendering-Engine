2389ba97d21dbf004f3e20061769352fe26d0c85
Felipe Andrioli <felipeandrioli56@hotmail.com>
2023-03-28 20:12:46 -0300

    Added the simulation of a low resolution display

--- src/display.c
+++ src/display.c
@@ -7,8 +7,8 @@ static SDL_Texture* color_buffer_texture = NULL;
 static uint32_t *color_buffer = NULL;
 static float *z_buffer = NULL;
 
-static int window_width = 800;
-static int window_height = 600;
+static int window_width = 320;
+static int window_height = 200;
 
 static render_options rendering_options;
 
@@ -21,13 +21,20 @@ bool initialize_window(void) {
     // Query and set fullscreen resolution
     SDL_DisplayMode display_mode;
     SDL_GetCurrentDisplayMode(0, &display_mode);
-    window_width = display_mode.w;
-    window_height = display_mode.h;
+    
+    //window_width = display_mode.w;
+    //window_height = display_mode.h;
+
+    int fullscreen_width = display_mode.w;
+    int fullscreen_height = display_mode.h;
+
+    window_width = fullscreen_width / 2;
+    window_height = fullscreen_height / 2;
 
     // Create a SDL Window
     window = SDL_CreateWindow("ScratchRenderingEngine.exe", 
-        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, window_width, 
-        window_height, SDL_WINDOW_BORDERLESS);
+        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, fullscreen_width, 
+        fullscreen_height, SDL_WINDOW_BORDERLESS);
 
     if (!window) {
         fprintf(stderr, "Error creating SDL window\n");
